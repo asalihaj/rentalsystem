b16a012fa926d31109e63aab61d26d0309b3a3d2
asalihaj <76753549+asalihaj@users.noreply.github.com>
2023-12-04 18:11:48 +0100

    Update ApplicationConfig.java

--- src/main/java/net/ubt/rentalsystem/config/ApplicationConfig.java
+++ src/main/java/net/ubt/rentalsystem/config/ApplicationConfig.java
@@ -40,7 +40,7 @@ public class ApplicationConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://booking-nine-nu.vercel.app/");
+        config.addAllowedOrigin("*");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
